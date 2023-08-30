da3df0a1e3ded9514f91d9dca6891b61cb95eeae
Özgür Atak <45077422+ozguratak@users.noreply.github.com>
2023-08-30 12:58:52 +0300

    Task 3 Development
    - Counter Bugs are fixed,
    - All functions are usable for counter now.
    - with these improvements, users can select minute and second values for counter via picker view or typing directly then press the start for starting the counter. When counter start to counting start button change as pause button with red background color. If user wants to pause the counter, just press the pause button then counter will be stoped. If the user wants to reset counter just pressing on reset button is enough.
    
    Remaining Imrovements;
    
    - Notification Manager will be build and integrate the app,
    - testing

--- Task3/task3_app/ViewControllers/ViewController.swift
+++ Task3/task3_app/ViewControllers/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     private let pickerViewData = Array(0...59)
-    
     private var timeVM = TimeViewModel()
+    let timePickerView = UIPickerView()
+    let startButton = UIButton()
     
     lazy var timeTextField: UITextField = {
         let timeTextField = BindingTextField()
@@ -34,28 +35,42 @@ class ViewController: UIViewController {
     }
     
     @objc func startButtonAction() {
-        print("start action")
-        print(self.timeVM.counterSeconds.value)
-        // Kullanım örneği
-        if let time = Int(self.timeVM.counterSeconds.value) {
-            timeVM.startCountdown(seconds: time, updateHandler: { secondsRemaining in
+        switch startButton.title(for: .normal) {
+        case "Pause":
+            timeVM.countdown(state: .pause) { remainingTime in
+                self.timeTextField.text = "Paused at \(remainingTime)"
+                self.startButton.backgroundColor = UIColor.systemGreen
+                self.startButton.setTitle("Resume", for: .normal)
+            } completion: {
+                self.startButton.backgroundColor = UIColor.systemGreen
+                self.startButton.setTitle("Resume", for: .normal)
+            }
+        default:
+            timeVM.countdown(state: .start) { secondsRemaining in
                 self.timeTextField.text = "\(secondsRemaining)"
-            }) {
+                self.startButton.backgroundColor = UIColor.systemRed
+                self.startButton.setTitle("Pause", for: .normal)
+            } completion: {
                 self.timeTextField.text = "Timer has elapsed!"
+                self.startButton.setTitle("Start", for: .normal)
+                self.startButton.backgroundColor = UIColor.systemGreen
             }
         }
-        
+
     }
     
     @objc func resetButtonAction() {
         DispatchQueue.main.async {
-            self.timeTextField.text = "0"
-            self.timePickerView.selectRow(0, inComponent: 0, animated: true)
-            self.timePickerView.selectRow(0, inComponent: 1, animated: true)
-            
+            self.timeVM.countdown(state: .reset) { remainingTime in
+                self.timeTextField.text = "Counter Reset"
+                self.startButton.backgroundColor = UIColor.systemGreen
+                self.startButton.setTitle("Start", for: .normal)
+            } completion: {
+                self.startButton.backgroundColor = UIColor.systemGreen
+                self.startButton.setTitle("Start", for: .normal)
+            }
         }
     }
-    let timePickerView = UIPickerView()
     
     private func setupUI() {
         let minutes = UILabel()
@@ -73,13 +88,12 @@ class ViewController: UIViewController {
         headersStackView.widthAnchor.constraint(equalToConstant: 120).isActive = true
         headersStackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
-        
         self.timePickerView.delegate = self
         self.timePickerView.dataSource = self
         self.timePickerView.widthAnchor.constraint(equalToConstant: 120).isActive = true
         self.timePickerView.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
-        let startButton = UIButton()
+        
         startButton.setTitle("Start", for: .normal)
         startButton.backgroundColor = UIColor.systemGreen
         startButton.layer.cornerRadius = 12
@@ -108,8 +122,6 @@ class ViewController: UIViewController {
         stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         stackView.widthAnchor.constraint(equalToConstant: 200).isActive = true
     }
-
-
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
--- Task3/task3_app/ViewModels/TimeViewModel.swift
+++ Task3/task3_app/ViewModels/TimeViewModel.swift
@@ -7,25 +7,54 @@
 
 import Foundation
 class TimeViewModel {
-    var counterSeconds = Dynamic("")
-    
-    func startCountdown(seconds: Int, updateHandler: @escaping (Int) -> Void, completion: @escaping () -> Void) {
-        var remainingTime = seconds
-        
-        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            updateHandler(remainingTime)
-            
-            if remainingTime > 0 {
-                remainingTime -= 1
-            } else {
-                timer.invalidate()
+    var counterSeconds = Dynamic("0")
+   private var counterRemaining: Int?
+    var currentAction: String?
+    private var timer: Timer?
+    func countdown(state: TimerState, updateHandler: @escaping (Int) -> Void, completion: @escaping () -> Void) {
+        currentAction = state.rawValue
+        if let time = Int(counterSeconds.value) {
+            switch state {
+            case .start:
+                var remainingTime = time
+                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                    updateHandler(remainingTime)
+                    if remainingTime > 0 {
+                        remainingTime -= 1
+                        self.counterSeconds.value = String(remainingTime)
+                    } else {
+                        timer.invalidate()
+                        completion()
+                    }
+                }
+                if let timer = timer {
+                    timer.fire()
+                } else {
+                    fatalError("Timer was not found!")
+                }
+            case .pause:
+                // Eğer pause işlemi yapılıyorsa, remainingTime değerini güncelleyin.
+                
+                if let remaining = Int(self.counterSeconds.value) {
+                    self.counterRemaining = remaining
+                    self.counterSeconds.value = String(describing: remaining)
+                    timer?.invalidate()
+                    updateHandler(remaining)
+                    completion()
+                }
+            case .reset:
+                timer?.invalidate()
+                // Reset işlemi yapıldığında counterSeconds ve counterRemaining değerlerini sıfırlayın.
+                self.counterSeconds.value = "0"
+                self.counterRemaining = nil
                 completion()
             }
         }
-        
-        // Timer'ı çalıştır
-        timer.fire()
     }
 
-
+}
+enum TimerState: String, CaseIterable {
+    case start = "Start"
+    case pause = "Pause"
+    case reset = "Reset"
 }
